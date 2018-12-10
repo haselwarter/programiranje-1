f96caff69144a2ed709955e929ab3d861b18466b
Philipp G. Haselwarter <philipp@haselwarter.org>
2018-12-10 11:19:08 +0100

    Tut: First half of search trees

--- 8-iskalna-drevesa/exercises/search_trees.ml
+++ 8-iskalna-drevesa/exercises/search_trees.ml
@@ -5,7 +5,7 @@
  trees, which are either empty or they contain some data and two (possibly
  empty) subtrees. We assume no further structure of the trees.
 [*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
-
+type 'a tree = Empty | Node of 'a * 'a tree * 'a tree
 
 (*----------------------------------------------------------------------------*]
  We define a test case for simpler testing of functions. The test case
@@ -17,8 +17,12 @@
        /   / \
       0   6   11
 [*----------------------------------------------------------------------------*)
-
-
+let leaf x = Node (x, Empty, Empty)
+let test_tree =
+     let left = Node (2, leaf 0, Empty)
+     and right = Node (7, leaf 6, leaf 11) 
+     in
+     Node (5, left, right)
 (*----------------------------------------------------------------------------*]
  The function [mirror] returns a mirrored tree. When applied to our test tree
  it returns
@@ -33,7 +37,13 @@
  Node (Node (Node (Empty, 11, Empty), 7, Node (Empty, 6, Empty)), 5,
  Node (Empty, 2, Node (Empty, 0, Empty)))
 [*----------------------------------------------------------------------------*)
-
+let rec mirror = function 
+     | Empty -> Empty
+     | Node (x, left, right) -> 
+          let new_left = mirror right in
+          let new_right = mirror left 
+          in 
+          Node (x, new_left, new_right)
 
 (*----------------------------------------------------------------------------*]
  The function [height] returns the height (or depth) of the tree and the
@@ -44,7 +54,19 @@
  # size test_tree;;
  - : int = 6
 [*----------------------------------------------------------------------------*)
-
+let rec height = function 
+| Empty -> 0
+| Node (_, l, r) -> let hl = height l 
+                    and hr = height r in
+                    let height_subtrees = max hl hr in 
+                    1 + height_subtrees
+let rec size = function 
+| Empty -> 0
+| Node (_, l, r) -> let sl = size l 
+                    and sr = size r in
+                    let size_subtrees = sl + sr in 
+                    1 + size_subtrees
+ 
 
 (*----------------------------------------------------------------------------*]
  The function [map_tree f tree] maps the tree into a new tree with nodes that
@@ -55,7 +77,13 @@
  Node (Node (Node (Empty, false, Empty), false, Empty), true,
  Node (Node (Empty, true, Empty), true, Node (Empty, true, Empty)))
 [*----------------------------------------------------------------------------*)
-
+let rec map_tree f = function 
+     | Empty -> Empty
+     | Node (x, l, r) -> 
+          let l = map_tree f l 
+          and r = map_tree f r
+          and x = f x in 
+          Node (x, l, r)
 
 (*----------------------------------------------------------------------------*]
  The function [list_of_tree] returns the list of all elements in the tree. If
@@ -64,7 +92,9 @@
  # list_of_tree test_tree;;
  - : int list = [0; 2; 5; 6; 7; 11]
 [*----------------------------------------------------------------------------*)
-
+let rec list_of_tree = function 
+  | Empty -> []
+  | Node (x, l, r) -> (list_of_tree l) @ [x] @ (list_of_tree r)
 
 (*----------------------------------------------------------------------------*]
  The function [is_bst] checks wheter a tree is a binary search tree (BST). 
@@ -76,7 +106,13 @@
  # test_tree |> mirror |> is_bst;;
  - : bool = false
 [*----------------------------------------------------------------------------*)
-
+let is_bst tree =
+     let rec is_sorted = function 
+       | [] | [_] -> true
+       | x :: (y :: _ as t) -> x < y && is_sorted t 
+       in 
+     let list = list_of_tree tree in 
+     is_sorted list
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  In the remaining exercises we assume that all trees are binary search trees.
@@ -91,7 +127,17 @@
  # member 3 test_tree;;
  - : bool = false
 [*----------------------------------------------------------------------------*)
+let rec insert x = function 
+  | Empty -> leaf x
+  | Node (y, l, r) when x < y -> Node (y, insert x l, r)
+  | Node (y, l, r) as t when x = y -> t
+  | Node (y, l, r) (* when x > y *) -> Node (y, l, insert x r)
 
+let rec member x = function
+  | Empty -> false 
+  | Node (y, l, _) when x < y -> member x l 
+  | Node (y, _, r) when x > y -> member x r 
+  | Node (z, _, _) -> x = z
 
 (*----------------------------------------------------------------------------*]
  The function [member2] does not assume that the tree is a bst.
@@ -99,7 +145,9 @@
  Note: Think about the differences of time complexity for [member] and 
  [member2] assuming an input tree with n nodes and depth of log(n). 
 [*----------------------------------------------------------------------------*)
-
+let rec member2 x = function
+  | Empty -> false 
+  | Node (y, l, r) -> x = y || (member2 x l) || member2 x r
 
 (*----------------------------------------------------------------------------*]
  The function [succ] returns the successor of the root of the given tree, if
@@ -113,7 +161,15 @@
  # pred (Node(Empty, 5, leaf 7));;
  - : int option = None
 [*----------------------------------------------------------------------------*)
-
+let succ bst =
+     let rec min = function 
+       | Empty -> None
+       | Node (x, Empty, _) -> Some x
+       | Node (_, l, _) -> min l 
+     in 
+     match bst with 
+     | Empty -> None 
+     | Node (_, _, r) -> min r
 
 (*----------------------------------------------------------------------------*]
  In lectures you two different approaches to deletion, using either [succ] or
